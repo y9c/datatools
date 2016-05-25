bca80be8e3a2134241ff2bf47a234b3c7de88264
yech1990 <yech1990@gmail.com>
2016-05-25 22:33:10 +0800

    update

--- dataiku/pyenv/lib/python2.7/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import site, pkg_resources
-site.addsitedir('/data1/tools/dataiku-dss-2.0.1/python.packages')
-pkg_resources.fixup_namespace_packages('/data1/tools/dataiku-dss-2.0.1/python.packages')
